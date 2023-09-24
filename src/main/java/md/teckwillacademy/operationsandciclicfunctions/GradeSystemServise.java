4d34a2365d3a832df6cdbb568d3357db366a84f0
Mariana <bulargamariana@mail.ru>
2023-09-24 23:24:55 +0300

    Added the Implementation of Java  Operation,Cyclie Functions and Control Stretures

---
+++ src/main/java/md/teckwillacademy/operationsandciclicfunctions/GradeSystemServise.java
@@ -0,0 +1,22 @@
+package md.teckwillacademy.operationsandciclicfunctions;
+
+public class GradeSystemServise {
+    public static void main(String[] args) {
+        int score = 59;
+        if (score >= 90){
+            System.out.println("The grade is A");
+        } else if (score >= 80) {
+            System.out.println("This grade is B");
+        } else if (score >= 70) {
+            System.out.println("This grade is C");
+        } else if (score >= 60) {
+            System.out.println("This grade is D");
+        }else {
+            System.out.println("This grade is failed");
+
+
+        }
+
+        }
+    }
+
